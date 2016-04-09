369f7f023788165be6c1dcc516ee6c8991edcd39
XeryusTC <armadillo@onenetbeyond.org>
2016-04-09 23:59:30 +0200

    Use list comprehensions instead of clumsy loops in RRT*2

--- rrtstar2.py
+++ rrtstar2.py
@@ -10,35 +10,25 @@ def rrtstar(g, start, goal, max_iter=10000):
         # Pick a random point in the grid
         q = (random.randrange(g.width), random.randrange(g.height))
         # Find the cell in the tree closest to random point
-        closest_tree_node = start
-        dist = grid.dist(q, closest_tree_node)
-        for cell in nodes.keys():
-            if dist > grid.dist(q, cell):
-                closest_tree_node = cell
-                dist = grid.dist(q, cell)
+        dists = {n: grid.dist(q, n) for n in nodes}
+        closest_tree_node = min(dists, key=lambda k: dists[k])
         # Find a neighbour of closest that is even closer to q
-        best_new_node = closest_tree_node
-        for n in g.neighbours(*closest_tree_node):
-            if dist > grid.dist(q, n):
-                best_new_node = n
-                dist = grid.dist(q, n)
+        dists = {n: grid.dist(q, n) for n in g.neighbours(*closest_tree_node)}
+        best_new_node = min(dists, key=lambda k: dists[k])
         if best_new_node in nodes:
             continue # skip nodes if they are already in the tree
         # Find the neighbour of the best cell with the lowest cost
-        best_parent = start
-        cost = 10000
-        for n in g.neighbours(*best_new_node):
-            if n not in nodes:
-                continue
-            if nodes[n]['cost'] + grid.cost(best_new_node, n) < cost:
-                best_parent = n
-                cost = nodes[n]['cost'] + grid.cost(best_new_node, n)
+        costs = {n: nodes[n]['cost'] + grid.cost(best_new_node, n) for n in
+            g.neighbours(*best_new_node) if n in nodes}
+        best_parent = min(costs, key=lambda k: costs[k])
         # Add new node
-        cost = nodes[best_parent]['cost'] + grid.cost(best_parent, best_new_node)
+        cost = nodes[best_parent]['cost'] + grid.cost(best_parent,
+            best_new_node)
         nodes[best_new_node] = {'cost': cost, 'parent': best_parent}
         if best_new_node == goal:
             return _reconstruct_path(goal, nodes), _build_edge_list(nodes)
-        # Check the neighbours if their cost can be decreased by connecting to the new node
+        # Check the neighbours if their cost can be decreased by
+        # connecting to the new node
         for n in g.neighbours(*best_new_node):
             if n not in nodes:
                 continue
@@ -52,7 +42,8 @@ def rrtstar(g, start, goal, max_iter=10000):
 def _update_child_cost(nodes, parent):
     for node in nodes:
         if nodes[node]['parent'] == parent:
-            nodes[node]['cost'] = nodes[parent]['cost'] + grid.cost(parent, node)
+            nodes[node]['cost'] = nodes[parent]['cost'] + grid.cost(parent,
+                node)
             _update_child_cost(nodes, node)
 
 def _reconstruct_path(goal, nodes):
@@ -65,7 +56,8 @@ def _reconstruct_path(goal, nodes):
     return path
 
 def _build_edge_list(nodes):
-    return [(nodes[node]['parent'], node) for node in nodes if nodes[node]['parent'] != None]
+    return [(nodes[node]['parent'], node) for node in nodes
+        if nodes[node]['parent'] != None]
 
 if __name__ == '__main__':
     g, start, goal = util.generate_problem(32, 32, 0.2)
